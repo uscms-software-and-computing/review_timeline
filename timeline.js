dfc2c9f48ff539952fb87454a10e2bf117314448
Rob Tuck <robtuck@gmail.com>
2021-08-19 10:34:40 -0400

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -1,58 +1,78 @@
 var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
 
 var events = [
-  {
-    "Task Name": "10% Network Challenge",
-    "Planned Finish Date": "12/01/21"
-  },
-  {
-    "Task Name": "WLCG 2022 pledge deployed",
-    "Planned Finish Date": "03/31/22"
-  },
-  {
-    "Task Name": "CMS detector closed. Start of LHC beam commissioning for Run 3.",
-    "Planned Finish Date": "03/31/22"
-  },
-  {
-    "Task Name": "Start of LHC beam operation and CMS data taking for Run 3.",
-    "Planned Finish Date": "06/30/22"
-  },
-  {
-    "Task Name": "WLCG 2023 pledge deployed",
-    "Planned Finish Date": "03/31/23"
-  },
-  {
-    "Task Name": "30% Network Challenge",
-    "Planned Finish Date": "12/01/23"
-  },
-  {
-    "Task Name": "WLCG 2024 pledge deployed",
-    "Planned Finish Date": "04/01/24"
-  },
-  {
-    "Task Name": "CMS Computing TDR Submitted",
-    "Planned Finish Date": "04/01/24"
-  },
-  {
-    "Task Name": "CMS Computing TDR Review",
-    "Planned Finish Date": "09/30/24"
-  },
-  {
-    "Task Name": "WLCG 2025 pledge deployed",
-    "Planned Finish Date": "03/31/25"
-  },
-  {
-    "Task Name": "60% Network Challenge",
-    "Planned Finish Date": "12/01/25"
-  },
-  {
-    "Task Name": "WLCG 2026 pledge deployed",
-    "Planned Finish Date": "03/31/26"
-  },
-  {
-    "Task Name": "100% Network Challenge",
-    "Planned Finish Date": "12/01/27"
-  }
+ {
+   "Task Name": "WLCG 2022 pledge deployed",
+   "Planned Finish Date": "3/31/22"
+ },
+ {
+   "Task Name": "CMS detector closed. Start of LHC beam commissioning for Run 3.",
+   "Planned Finish Date": "3/31/22"
+ },
+ {
+   "Task Name": "Start of LHC beam operation and CMS data taking for Run 3.",
+   "Planned Finish Date": "6/30/22"
+ },
+ {
+   "Task Name": "WLCG 2023 pledge deployed",
+   "Planned Finish Date": "3/31/23"
+ },
+ {
+   "Task Name": "WLCG 2024 pledge deployed",
+   "Planned Finish Date": "4/1/24"
+ },
+ {
+   "Task Name": "CMS Computing TDR Submitted",
+   "Planned Finish Date": "4/1/24"
+ },
+ {
+   "Task Name": "CMS Computing TDR Review",
+   "Planned Finish Date": "9/30/24"
+ },
+ {
+   "Task Name": "WLCG 2025 pledge deployed",
+   "Planned Finish Date": "3/31/25"
+ },
+ {
+   "Task Name": "WLCG 2026 pledge deployed",
+   "Planned Finish Date": "3/31/26"
+ },
+ {
+   "Task Name": "10% Network Challenge",
+   "Planned Finish Date": "12/1/21"
+ },
+ {
+   "Task Name": "30% Network Challenge",
+   "Planned Finish Date": "12/1/23"
+ },
+ {
+   "Task Name": "60% Network Challenge",
+   "Planned Finish Date": "12/1/25"
+ },
+ {
+   "Task Name": "100% Network Challenge",
+   "Planned Finish Date": "12/1/27"
+ },
+ {
+   "Task Name": "Common software projects and WLCG software",
+   "Planned Finish Date": "11/1/21"
+ },
+ {
+   "Task Name": "HL-LHC Computing CDRs for ATLAS and CMS",
+   "Planned Finish Date": "10/3/22"
+ },
+ {
+   "Task Name": "HSF, Common Software Projects, WLCG facilities",
+   "Planned Finish Date": "10/2/23"
+ },
+ {
+   "Task Name": "Delivery of the HL-LHC ATLAS and CMS Computing TDR",
+   "Planned Finish Date": "4/1/24"
+ },
+ {
+   "Task Name": "Review of the HL-LHC ATLAS and CMS Computing TDR",
+   "Planned Finish Date": "10/1/24"
+ }
 ];
 
 var html = '';
